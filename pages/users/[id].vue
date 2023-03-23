e404d709562bd022f6513db3bf1fbb3f74678c5e
ynakayu <9111754+ynakayu@users.noreply.github.com>
2023-03-23 16:57:23 +0900

    04-05-val idate

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -3,3 +3,10 @@
     <h1>users/{{ $route.params.id }}を表示中</h1>
   </div>
 </template>
+<script setup>
+  definePageMeta({
+    validate: async(route) => {
+      return /^\d+$/.test(route.params.id)
+    }
+  })
+</script>
